4eb85aaabd1cd4b907134d23d8f66448b6432381
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-11-22 21:25:18 +0530

    #include<stdio.h> int main() {     int n,c;     while(1)     {         scanf("%d",&n);         if(n!=-1)         {
                printf("%d
    ",n*n);
            }else {
                break;
            }
            
            
        }
    }

---
+++ Infinite.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,c;
+    while(1)
+    {
+        scanf("%d",&n);
+        if(n!=-1)
+        {
+            
+            printf("%d
+",n*n);
+        }else {
+            break;
+        }
+        
+        
+    }
+}
