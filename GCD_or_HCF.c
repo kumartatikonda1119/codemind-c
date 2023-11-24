d9ad46ec8574d7fc3b20c5983db61396d892dad6
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-11-24 10:45:34 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e=0,i;     scanf("%d%d",&a,&b);     for (i=1;i<=a*b;i++)     {         if(a%i==0&&b%i==0)         {             if(i>e)             {                 e=i;
                }
            }
            
        }printf("%d",e);
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e=0,i;
+    scanf("%d%d",&a,&b);
+    for (i=1;i<=a*b;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            if(i>e)
+            {
+                e=i;
+                
+            }
+        }
+        
+    }printf("%d",e);
+}
