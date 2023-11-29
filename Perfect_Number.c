5b9b7be7291d03138de16ba273aecc1f6643a8fa
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-11-29 21:09:50 +0530

    #include<stdio.h> int main() {     int n,c=0,i;     scanf("%d",&n);     for (i=1;i<n;i++)     {         if(n%i==0)         {             c=c+i;         }     }if(c==n)     {         printf("True");     }else {         printf("False");     } }

---
+++ Perfect_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,i;
+    scanf("%d",&n);
+    for (i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            c=c+i;
+        }
+    }if(c==n)
+    {
+        printf("True");
+    }else {
+        printf("False");
+    }
+}
