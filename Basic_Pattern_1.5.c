3b087e6392988b56e5f1c932fb70fa90a51e5e2a
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-12-10 17:06:34 +0530

    #include<stdio.h> int main() {     int a,i,j,k,l;     scanf("%d",&a);     for(i=65;i<=65+a-1;i++)     {         for(j=1;j<=a;j++)         {             printf("%c ",i);         }printf(" ");     } }

--- Basic_Pattern_1.5.c
+++ Basic_Pattern_1.5.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,k,l,a,o;
-	scanf("%d",&a);
-	o=a;
-	for(i=0;i<1;i++)
-	{
-		for(j=65;j<=65+a-1;j++)
-		{
-			l=j;
-			for(k=1;k<=o;k++)
-			{
-				printf("%c ",l);
-			}printf("
+    int a,i,j,k,l;
+    scanf("%d",&a);
+    for(i=65;i<=65+a-1;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            printf("%c ",i);
+        }printf("
 ");
-		}
-	}
-	
+    }
 }
