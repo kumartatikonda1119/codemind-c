399b7b79ae34ae9ff3c149cdf1abaff2980f0e7d
kumartatikonda1119 <147791707+kumartatikonda1119@users.noreply.github.com>
2023-12-16 22:56:41 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],c;     for( int i=0;i<a;i++)     {         scanf("%d",&arr[i]);
            if(arr[i]%2!=0)
            {
               c=i; 
            }
        }printf("%d",c);
    }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],c;
+    for( int i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+        if(arr[i]%2!=0)
+        {
+           c=i; 
+        }
+    }printf("%d",c);
+}
