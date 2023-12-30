e714620d9e10dae49c3cab3838c1897317e416f9
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2023-12-30 17:11:46 +0530

    #include<stdio.h> int main() {     int x,c=0;     scanf("%d",&x);     char a[1000];
        for(int i=1;i<=x;i++){
             scanf("%s",&a);
        
        {
            if(a[1]=='+')
            {
                c++;
                continue;
                
                
            }else {
                c--;
                continue;
                
                
            }
        }
        }
        printf("%d",c);
        
        
    }

---
+++ Bits++.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int x,c=0;
+    scanf("%d",&x);
+    char a[1000];
+   
+    for(int i=1;i<=x;i++){
+         scanf("%s",&a);
+    
+    {
+        if(a[1]=='+')
+        {
+            c++;
+            continue;
+            
+            
+        }else {
+            c--;
+            continue;
+            
+            
+        }
+    }
+    }
+    printf("%d",c);
+    
+    
+}
