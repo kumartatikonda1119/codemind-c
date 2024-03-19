0c7b0d702370717e84959b7a66fa2da4f3042043
Kumar Tatikonda <147791707+kumartatikonda1119@users.noreply.github.com>
2024-03-19 18:52:12 +0530

    #include<iostream> using namespace std;
    int main()
    {
        int a,b,c=0;
        cin>>a>>b;
        for(int i=a;i<=b;i++)
        {
            if(i%3==0)
            {
                c++;
                
                
            }
        }
        cout<<c;
        
    }

--- Count_Multiples_of_3_between_M_,N.c
+++ Count_Multiples_of_3_between_M_,N.c
@@ -1,13 +1,19 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
+
 int main()
 {
-    int a,b,i,c=0;
-    scanf("%d%d",&a,&b);
-    for (i=a;i<=b;i++)
+    int a,b,c=0;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++)
     {
         if(i%3==0)
         {
             c++;
+            
+            
         }
-    }printf("%d",c);
+    }
+    cout<<c;
+    
 }
